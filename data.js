439f9e4b8fd7008347694282e078265d1abe3f54
erebos928 <shahin.behrooz@gmail.com>
2019-03-10 17:38:30 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+const a = new Promise(function(resolve,reject){
+  const data = [{"id":"55432","startDate":"2019-03-01"},
+{"id":"55433","startDate":"2020-05-01"},
+{"id":"55434","startDate":"2018-03-21"}];
+  resolve(data);
+});
